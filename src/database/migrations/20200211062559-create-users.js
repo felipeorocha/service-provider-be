bc49d7577612038a6785265c78d6701c1f25f86a
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-02-12 01:32:58 -0300

    Database connection loader

--- src/database/migrations/20200211062559-create-users.js
+++ src/database/migrations/20200211062559-create-users.js
@@ -1,11 +1,5 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-     */
     return queryInterface.createTable('users', {
       id: {
         type: Sequelize.INTEGER,
@@ -43,12 +37,6 @@ module.exports = {
   },
 
   down: (queryInterface) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-     */
     return queryInterface.dropTable('users');
   },
 };
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: "I'm ok" });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Felipe Rocha',
+    email: 'felieo.rocha@hotmail.com',
+    password_hash: '23232323',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
