73a3fd0f31f9f17cf9b0bea036f8e888d60b09e1
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-02-16 03:39:10 -0300

    Auth: user authentication and update implemented. Algo, auth middleware added

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '581bea143ecf0e80fd4923f7d8ed9699',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+routes.put('/users', authMiddleware, UserController.update);
 
 export default routes;
