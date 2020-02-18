df8433fd391a20e1e75c3e83637d990af160e715
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-02-18 01:33:03 -0300

    Avatar: user avatar file upload implemented

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,9 @@
 import Sequelize from 'sequelize';
 import User from '../app/models/User';
+import File from '../app/models/File';
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File];
 
 class Database {
   constructor() {
@@ -13,6 +14,7 @@ class Database {
     this.connection = new Sequelize(databaseConfig);
 
     models.map(model => model.init(this.connection));
+    models.map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -13,8 +14,6 @@ const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 routes.put('/users', authMiddleware, UserController.update);
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', FileController.store);
 
 export default routes;
