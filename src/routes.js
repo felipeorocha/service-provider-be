16d672eaee48eb92ddaf9eb327f115f3c5db40fb
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-02-23 15:45:01 -0300

    Appointment: appointment pagination and schedule list

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -17,6 +18,7 @@ routes.post('/users', UserController.store);
 routes.get('/providers', ProviderController.index);
 routes.post('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.get('/scheduler', ScheduleController.index);
 routes.post('/sessions', SessionController.store);
 routes.put('/users', authMiddleware, UserController.update);
 routes.post('/files', upload.single('file'), FileController.store);
