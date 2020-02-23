986839fbe56b03322d31fb2e15bbea84c9a0dd3b
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-02-23 04:32:57 -0300

    Improvs: files and provider routes defined. User and file models improvement

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -12,8 +13,9 @@ const routes = Router();
 const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
+routes.get('/providers', ProviderController.index);
 routes.post('/sessions', SessionController.store);
 routes.put('/users', authMiddleware, UserController.update);
-routes.post('/files', FileController.store);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
