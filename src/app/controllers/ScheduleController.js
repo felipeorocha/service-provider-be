44180598f98e3da41270810f4ce48cd2c9b9d3cf
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-02-25 19:46:34 -0300

    Setup cors and user avatart response

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -30,6 +30,13 @@ class ScheduleControler {
           ],
         },
       },
+      include: [
+        {
+          model: User,
+          as: 'user',
+          attributes: 'name',
+        },
+      ],
       order: ['date'],
     });
 
