2a691e6d32bf5ef5e5d289f02cceb1b36b4b08f0
Felipe Rocha <felipeo.rocha@hotmail.com>
2020-02-25 19:47:43 -0300

    Code improvement

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -38,7 +38,12 @@ class SessionController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name, avatar, provider } = user;
+    const {
+      id,
+      name,
+      avatar,
+      provider,
+    } = user;
 
     return res.json({
       user: {
